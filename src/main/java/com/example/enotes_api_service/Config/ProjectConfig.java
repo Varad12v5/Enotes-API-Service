5fdf3431be34757ed730b151b669a369da7a20c9
Varad12v5 <vbrahmapurkar@gmail.com>
2025-02-01 12:44:29 +0530

    implemeted Hardcoded Auditing

--- src/main/java/com/example/enotes_api_service/Config/ProjectConfig.java
+++ src/main/java/com/example/enotes_api_service/Config/ProjectConfig.java
@@ -3,6 +3,7 @@ package com.example.enotes_api_service.Config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 
 @Configuration
 public class ProjectConfig {
@@ -11,5 +12,11 @@ public class ProjectConfig {
     public ModelMapper mapper()
     {
         return new ModelMapper();
+    }
+
+    @Bean
+    public AuditorAware<Integer> auditorAware()
+    {
+        return new AuditAwareConfig();
     }
 }
--- src/main/java/com/example/enotes_api_service/EnotesApiServiceApplication.java
+++ src/main/java/com/example/enotes_api_service/EnotesApiServiceApplication.java
@@ -4,8 +4,10 @@ import com.example.enotes_api_service.Dto.CategoryDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class EnotesApiServiceApplication {
 
     public static void main(String[] args) {
